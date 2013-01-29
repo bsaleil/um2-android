1e91044b7f7e4be1384de4ba2be8eb7af35807c6
bapt <bapt@linux-miv6.site>
2013-01-29 01:43:47 +0100

    Simple vent

---
+++ Projet-android/src/com/um2/android/SimpleEvent.java
@@ -0,0 +1,44 @@
+package com.um2.android;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class SimpleEvent
+{
+	private String summary;
+	private int numBuilding;
+	private String startDay; // jj/mm/aaaa
+	private String endDay; // jj/mm/aaaa
+	private int minutesStart;
+	private int minutesEnd;
+	
+	public void setADTSummary(String s)
+	{
+		summary = s;
+	}
+	
+	public void setADTBuilding(int num)
+	{
+		numBuilding = num;
+	}
+	
+	public void setADTStartDay(String date)
+	{
+		startDay = date;
+	}
+	
+	public void setADTEndDay(String date)
+	{
+		endDay = date;
+	}
+	
+	public void etADTMinutesStart(int minutes)
+	{
+		minutesStart = minutes;
+	}
+	
+	public void setADTMinutesEnd(int minutes)
+	{
+		minutesEnd = minutes;
+	}
+}
